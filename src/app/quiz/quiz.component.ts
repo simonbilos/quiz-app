de6ff7de31fae8f3025f6df4a97c92242ec6a57a
Šimon Biloš <bilos.simon@gmail.com>
2025-08-22 16:05:40 +0200

    show only one question

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, inject, effect } from "@angular/core";
+import { Component, signal, inject, effect, computed } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { QuizQuestionComponent } from "../quiz-question/quiz-question.component";
 
@@ -20,6 +20,9 @@ interface QuizAnswer {
 export class QuizComponent {
   private http = inject(HttpClient);
   questions = signal<Question[]>([]);
+  currentIndex = signal<number>(0);
+
+  currentQuestion = computed(() => this.questions()[this.currentIndex()]);
 
   constructor() {
     effect(() => {
@@ -27,5 +30,9 @@ export class QuizComponent {
         .get<Question[]>("/assets/quiz-question.json")
         .subscribe((data) => this.questions.set(data));
     });
+  }
+
+  onResponse() {
+    this.currentIndex.update((i) => i + 1);
   }
 }
