b49162191828eb98e734bc0686f4c4dea9e14723
Šimon Biloš <bilos.simon@gmail.com>
2025-08-22 15:50:34 +0200

    JSON parsing, and sending questions to QuizQuestionComponent

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject, effect } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { QuizQuestionComponent } from "../quiz-question/quiz-question.component";
+
+interface Question {
+  question: string;
+  answers: QuizAnswer[];
+}
+
+interface QuizAnswer {
+  answer: string;
+  isCorrect: boolean;
+}
 
 @Component({
-  selector: 'app-quiz',
-  imports: [],
-  templateUrl: './quiz.component.html',
-  styleUrl: './quiz.component.css'
+  selector: "app-quiz",
+  imports: [QuizQuestionComponent],
+  templateUrl: "./quiz.component.html",
 })
 export class QuizComponent {
+  private http = inject(HttpClient);
+  questions = signal<Question[]>([]);
 
+  constructor() {
+    effect(() => {
+      this.http
+        .get<Question[]>("/assets/quiz-question.json")
+        .subscribe((data) => this.questions.set(data));
+    });
+  }
 }
