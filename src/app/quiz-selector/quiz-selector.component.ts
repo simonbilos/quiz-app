4dc963c26ba1e8b9c3f4fbf3a9f641cffcd47abd
Šimon Biloš <bilos.simon@gmail.com>
2025-09-11 15:49:17 +0200

    new component for selecting which quiz wants to play

---
+++ src/app/quiz-selector/quiz-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-selector',
+  imports: [],
+  templateUrl: './quiz-selector.component.html',
+  styleUrl: './quiz-selector.component.css'
+})
+export class QuizSelectorComponent {
+
+}
